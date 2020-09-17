64666540319959b42c0004389c072860ae669a25
AlpoGIT <al.popoff.github@gmail.com>
2020-09-17 10:56:32 -1000

    correct stuffs

--- MergeSort.py
+++ MergeSort.py
@@ -1,12 +1,12 @@
 c = [50,65,99,87,74,63,76,100,5]
-print('initial:',c)
+print('initial:', c)
 
 # merge a and b
 def merge(a,b):
     'return ordered list, provided a and b are ordered'
-    print('merge:',a,b)
+    print('merge:', a, b)
     temp = []
-    i,j = 0,0
+    i, j = 0, 0
     while (i < len(a)) and (j < len(b)):
         if a[i]>b[j]:
             temp.append(b[j])
@@ -19,13 +19,13 @@ def merge(a,b):
 def mergesort(c):
     L = len(c)
     mid = L//2
-    while mid != 0:
+    if mid > 0:
         a = c[0:mid]
         b = c[mid:]
-        d = merge(mergesort(a),mergesort(b))
-        print('merged as:',d)
+        d = merge(mergesort(a), mergesort(b))
+        print('merged as:', d)
         print('-------------------------------------------')
         return d
     return c
 
-print('final result:',mergesort(c))
+print('final result:', mergesort(c))
